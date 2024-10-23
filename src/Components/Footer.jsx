33418303c45e36618640fab5acec7ce1974c00aa
Cristian Acosta <softeng.acostac@gmail.com>
2024-10-23 12:47:43 -0400

    second commit: added sections and footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+        <footer>
+            <p>
+                CFA Event Planner Organization. All rights Reserved.
+            </p>
+        </footer>
+        </>
+    )
+}
+export default Footer;
